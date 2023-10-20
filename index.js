e77af07d3bfa4bb2f32fc1b2a2294c259528d88f
Tanjil Ahamed <tanjil113355@gmail.com>
2023-10-20 16:25:20 +0600

    same bugs fixed

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
         const samsungCollection = database.collection("samsung");
         const sonyCollection = database.collection("sony");
         const intelCollection = database.collection("intel");
-        const clientCollection = database.collection("client");
+        const miCollection = database.collection("mi");
         const cartCollection = database.collection("cart");
 
 
@@ -62,21 +62,13 @@ async function run() {
             res.send(result)
         })
 
-
-        // client
-        app.post('/client', async (req, res) => {
-            const clientProduct = req.body
-            const result = await clientCollection.insertOne(clientProduct)
-            res.send(result)
-        })
-
-        app.get('/client', async (req, res) => {
-            const query = clientCollection.find()
-            const result = await query.toArray()
-            res.send(result)
-        })
-
         // apple
+        app.post('/apple', async (req, res) => {
+            const newProduct = req.body
+            const result = await iphoneCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
         app.get('/apple', async (req, res) => {
             const query = iphoneCollection.find()
             const result = await query.toArray()
@@ -110,11 +102,18 @@ async function run() {
 
 
         // google
+        app.post('/google', async (req, res) => {
+            const newProduct = req.body
+            const result = await googleCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
         app.get('/google', async (req, res) => {
             const query = googleCollection.find()
             const result = await query.toArray()
             res.send(result)
         })
+
         app.get('/google/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
@@ -143,6 +142,12 @@ async function run() {
 
 
         // samsung
+        app.post('/samsung', async (req, res) => {
+            const newProduct = req.body
+            const result = await samsungCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
         app.get('/samsung', async (req, res) => {
             const query = samsungCollection.find()
             const result = await query.toArray()
@@ -175,6 +180,11 @@ async function run() {
 
 
         // sony
+        app.post('/sony', async (req, res) => {
+            const newProduct = req.body
+            const result = await sonyCollection.insertOne(newProduct)
+            res.send(result)
+        })
         app.get('/sony', async (req, res) => {
             const query = sonyCollection.find()
             const result = await query.toArray()
@@ -208,6 +218,12 @@ async function run() {
 
 
         // intel
+        app.post('/intel', async (req, res) => {
+            const newProduct = req.body
+            const result = await intelCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
         app.get('/intel', async (req, res) => {
             const query = intelCollection.find()
             const result = await query.toArray()
@@ -234,6 +250,44 @@ async function run() {
                 }
             }
             const result = await intelCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+
+
+        // mii
+        app.post('/mi', async (req, res) => {
+            const newProduct = req.body
+            const result = await miCollection.insertOne(newProduct)
+            res.send(result)
+        })
+
+        app.get('/mi', async (req, res) => {
+            const query = miCollection.find()
+            const result = await query.toArray()
+            res.send(result)
+        })
+        app.get('/mi/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await miCollection.findOne(query)
+            res.send(result)
+        })
+        app.put('/mi/:id', async (req, res) => {
+            const id = req.params.id;
+            const update = req.body
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    name: update.name,
+                    brand: update.brand,
+                    price: update.price,
+                    photo: update.photo,
+                    rating: update.rating,
+                    type: update.type
+                }
+            }
+            const result = await miCollection.updateOne(filter, updateDoc)
             res.send(result)
         })
 
